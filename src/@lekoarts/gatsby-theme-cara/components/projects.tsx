c34ee9bc867ebecb00a27daf9b535335badb1af1
Ryan <redtadpolecreative@gmail.com>
2022-05-19 11:09:12 -0700

    adjusted divider offset for projects section of home page

--- src/@lekoarts/gatsby-theme-cara/components/projects.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/projects.tsx
@@ -14,7 +14,7 @@ const Projects = ({ offset, factor = 2 }: { offset: number; factor?: number }) =
       bg="linear-gradient(to right, SlateBlue 0%, DeepSkyBlue 100%)"
       sx={{ clipPath: `polygon(0 15%, 100% 25%, 100% 85%, 0 75%)` }}
       speed={-0.2}
-      offset={0.725}
+      offset={0.69}
       factor={factor}
     />
     <Content speed={0.4} offset={offset + 0.2} factor={factor}>
